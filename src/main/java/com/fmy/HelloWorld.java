8ebf61b618668a02fb93257d6b5f6d27c667bb42
moon_glb <imfmy_glb@foxmail.com>
2021-12-12 22:44:19 +0800

    second commit

--- src/main/java/com/fmy/HelloWorld.java
+++ src/main/java/com/fmy/HelloWorld.java
@@ -2,6 +2,6 @@ package com.fmy;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("hello world!,Ha,Ha");
     }
 }
