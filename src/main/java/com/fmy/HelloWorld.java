b0273d3fe06a79199e86337cf46e28d22097fc0d
moon_glb <imfmy_glb@foxmail.com>
2021-12-12 22:51:17 +0800

    5 commit by master

--- src/main/java/com/fmy/HelloWorld.java
+++ src/main/java/com/fmy/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world!,Ha,Ha,3");
         System.out.println("hello 4,by hot-fix");
+        System.out.println("hello 5 master");
     }
 }
