1cd631a5b2acb1c062eb38b600b672c06044d8dd
moon_glb <imfmy_glb@foxmail.com>
2021-12-12 22:45:11 +0800

    third commit

--- src/main/java/com/fmy/HelloWorld.java
+++ src/main/java/com/fmy/HelloWorld.java
@@ -2,6 +2,6 @@ package com.fmy;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world!,Ha,Ha");
+        System.out.println("hello world!,Ha,Ha,3");
     }
 }
